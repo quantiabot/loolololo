1bab084b29ca8b941dd93f6482b9ce6c0d598421
quantiabot <amogusaboba740@gmail.com>
2025-10-29 15:51:21 +0200

    Update package files for new version

--- spin.py
+++ spin.py
@@ -1,5 +1,6 @@
-import base64
-def encode(data: str) -> str:
-    return   base64.b64encode(data.encode()).encode()
-def decode(data: str) -> str:
-    return    base64.b64decode(data.encode()).decode()
+import random
+import string
+def spin(len: int):
+    n=string.ascii_letters+string.digits
+    return''.join(random.choices(n, k=len))  
+print(spin(1))
